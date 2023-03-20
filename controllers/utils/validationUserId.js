4d2cacc277efc800d7773c70ea6dd2409862e0e1
vinod4848 <vinodku4848@gmail.com>
2023-03-20 19:52:30 +0530

    added refreshToken handleRefreshToken errorHandler

--- controllers/utils/validationUserId.js
+++ controllers/utils/validationUserId.js
@@ -1,5 +1,4 @@
 const ObjectId = require('mongoose').Types.ObjectId;
-
 function isValidObjectId(id){
     
     if(ObjectId.isValid(id)){
@@ -10,11 +9,3 @@ function isValidObjectId(id){
     return false;
 }
 module.exports = {isValidObjectId};
-
-// const  mongoose  = require("mongoose");
-// const isValidObjectId =  (id) =>{
-//     const isValid = mongoose.Schema. Types.ObjectId.isValid(id);
-//     if(!isValid) throw new Error("This id is not valid");
-// };
-
-// module.exports = {isValidObjectId};
